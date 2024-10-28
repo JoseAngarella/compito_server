37b39c014460012a8560ca91c5dbbdf9c387e9a1
lai2-24 <lai2-24@itismeucci.com>
2024-10-28 12:56:15 +0100

    progetto base completato

--- src/main/java/com/example/MyThread.java
+++ src/main/java/com/example/MyThread.java
@@ -2,10 +2,10 @@ package com.example;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+
 
 public class MyThread extends Thread {
     Socket socket;
@@ -18,10 +18,29 @@ public class MyThread extends Thread {
         try {
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream())); // ascolto                                                                              // (ricevere)
             DataOutputStream out = new DataOutputStream(socket.getOutputStream()); // parla (invia)
-
+            String messaggioRicevuto;
+            ArrayList<String> lista=new ArrayList<>();
             do {
+                messaggioRicevuto=in.readLine();
+                switch (messaggioRicevuto) {
+                    case "?":
+                        for(int i=0; i<lista.size();i++){
+                            out.writeBytes((i+1)+": "+lista.get(i)+ "\n");
+                        }
+                        out.writeBytes("@"+"\n");
+                        break;
+                    case "!":
+                        break;
                 
-            } while (true);
+                    default:
+                        lista.add(messaggioRicevuto);
+
+                        out.writeBytes("OK\n");
+                        break;
+                }
+
+            } while ( !messaggioRicevuto.equals("!"));
+            socket.close();
 
         } catch (Exception e) {
             // TODO: handle exception
